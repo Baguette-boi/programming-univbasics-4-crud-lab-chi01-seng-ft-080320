882ab2f7c1353f4f58c68aacf03cb684a50174c3
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-11 01:41:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  alphabet = ['a','b','c','d']
+  
   alphabet << 'e'
-  p alphabet
+
 end
 
 def add_element_to_start_of_array(array, element)
